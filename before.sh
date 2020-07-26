a032168979fcaa78b088261893b0894f27649e4f
langonginc <langonginc@yeah.net>
2020-07-26 09:04:08 +0800

    Update 'dev'

--- before.sh
+++ before.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # g++ zff_main_ch.cpp -o zff-ch && g++ zff_main_en.cpp -o zff-en
-g++ zff_main_en.cpp -o /bin/zff
+g++ zff_main_en.cpp -o /usr/bin/zff
 # echo " >> Please set a UserName 请设定一个用户名";
 # read name;
 # echo $USER >> ~/.local/share/ohzff-zff/user.dll
