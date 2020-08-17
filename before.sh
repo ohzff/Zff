e90ab3b73fc00cce6a394df8e9db9994b576f17f
langonginc <langonginc@yeah.net>
2020-08-17 14:31:29 +0800

    Push

--- before.sh
+++ before.sh
@@ -2,7 +2,8 @@
 
 # g++ zff_main_ch.cpp -o zff-ch && g++ zff_main_en.cpp -o zff-en
 g++ zff_main_en.cpp -o /usr/bin/zff
-cp -r ../Zff /usr/share/ohzff-zff
+mkdir -p /usr/share/ohzff-zff
+cp -r ../Zff/* /usr/share/ohzff-zff
 # echo " >> Please set a UserName 请设定一个用户名";
 # read name;
 # echo $USER >> ~/.local/share/ohzff-zff/user.dll
